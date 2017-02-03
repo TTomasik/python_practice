b64498a0bdc7d2e620467f47ab4cb62b1a1edef5
Tomasz Tomasik <tomasz.szymon.tomasik@gmail.com>
2017-02-03 11:13:27 +0100

    done 3 exercises

--- codewars.py
+++ codewars.py
@@ -1,26 +1,49 @@
-numbers = [1, 10, 22, 4]
-def sum_two_smallest_numbers(numbers):
-         
+# #---EX1 -> finds two smallest integers and show sum of them
+# numbers = [1, 10, 22, 4]
+# def sum_two_smallest_numbers(numbers):
+#          
+#     
+#     print(numbers)
+#     numbers.sort()
+#     print(numbers)
+#     result = numbers[0]+numbers[1]
+#     print(result)
+#     return result
+#    
+# 
+# sum_two_smallest_numbers(numbers)
+
+# #---EX2 -> squares every single integer and join int to one integer at the end
+# num = 123
+# str(num)
+# list = []
+# result = []
+# def haha(liczby):
+#     for a in str(num):     
+#         b = int(a)**2
+#         list.append(b)
+#     return ''.join(str(x) for x in list)  
+#         
+# print(haha(num))
+
+#---EX3 -> makes first letter capital    
+quote = "cant wait till summer"
+def capital(quotes):
+    list = quotes.split()    
+    result = []
+    for word in list:               
+        result.append(word[0].upper()+word[1:])        
+    return " ".join(result)
+
+print(capital(quote))
+
+print(quote.title()) # this one line does the same that the whole function above
     
-    print(numbers)
-    numbers.sort()
-    print(numbers)
-    result = numbers[0]+numbers[1]
-    print(result)
-    return result
-   
-
-sum_two_smallest_numbers(numbers)
-
-num = 123
-str(num)
-list = []
-result = []
-def haha(liczby):
-    for a in str(num):     
-        b = int(a)**2
-        list.append(b)
-    return ''.join(str(x) for x in list)  
-        
-print(haha(num))
+    
+    
+    
+    
+    
+    
+    
     
