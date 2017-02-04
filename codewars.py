648374c9cb9f496ffb062b3b40bd3633f2cbe027
Tomasz Tomasik <tomasz.szymon.tomasik@gmail.com>
2017-02-04 20:24:58 +0100

    the best solution for exercise 14th

--- codewars.py
+++ codewars.py
@@ -214,17 +214,23 @@ import math
 # 
 # print(find_even_index(arr))
 
-#---EX14 - BETTER SOLUTION :)
-def find_even_index(lst):
-    left_sum = 0
-    right_sum = sum(lst)
-    for i, a in enumerate(lst):
-        right_sum -= a
-        if left_sum == right_sum:
+# #---EX14 - BETTER SOLUTION :)
+# def find_even_index(lst):
+#     left_sum = 0
+#     right_sum = sum(lst)
+#     for i, a in enumerate(lst):
+#         right_sum -= a
+#         if left_sum == right_sum:
+#             return i
+#         left_sum += a
+#     return -1
+
+#---EX14 - THE BEST SOLUTION!!!
+def find_even_index(arr):
+    for i in range(len(arr)):
+        if sum(arr[:i]) == sum(arr[i+1:]):
             return i
-        left_sum += a
     return -1
-    
     
     
     
